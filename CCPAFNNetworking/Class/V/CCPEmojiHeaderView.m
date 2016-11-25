72dab88e5b58813b0524a513f48d551dde385b05
CCP <335966179@qq.com>
2016-11-25 11:01:58 +0800

    添加下拉刷新

--- CCPAFNNetworking/Class/V/CCPEmojiHeaderView.m
+++ CCPAFNNetworking/Class/V/CCPEmojiHeaderView.m
@@ -16,16 +16,16 @@
     
     // 设置普通状态的动画图片
     NSMutableArray *idleImages = [NSMutableArray array];
-    for (NSUInteger i = 1; i<=55; i++) {
-        UIImage *image = [UIImage imageNamed:[NSString stringWithFormat:@"Expression_%zd", i]];
+    for (NSUInteger i = 0; i<=16; i++) {
+        UIImage *image = [UIImage imageNamed:[NSString stringWithFormat:@"LivePhotoOn_3_5_%zd", i]];
         [idleImages addObject:image];
     }
     [self setImages:idleImages forState:MJRefreshStateIdle];
     
     // 设置即将刷新状态的动画图片（一松开就会刷新的状态）
     NSMutableArray *refreshingImages = [NSMutableArray array];
-    for (NSUInteger i = 1; i<=22; i++) {
-        UIImage *image = [UIImage imageNamed:[NSString stringWithFormat:@"loading_7_%zd", i]];
+    for (NSUInteger i = 1; i<=16; i++) {
+        UIImage *image = [UIImage imageNamed:[NSString stringWithFormat:@"LivePhotoOn_3_5_%zd", i]];
         [refreshingImages addObject:image];
     }
     [self setImages:refreshingImages forState:MJRefreshStatePulling];
